3e58068b5b0962df27253328d601b58b466e7752
aakj2010 <aakj2010@gmail.com>
2022-08-16 20:24:57 +0530

    new1

---
+++ src/Portal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+import SideBar from './SideBar';
+import TopBar from './TopBar';
+
+function Portal() {
+    return (
+      
+    )
+}
+
+export default Portal;
