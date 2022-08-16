5867ec88790bc31e746f2e6896906883a96c352f
aakj2010 <aakj2010@gmail.com>
2022-08-16 20:57:01 +0530

    1234

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,34 @@ import TopBar from './TopBar';
 import Users from './Users';
 import Products from './Products';
 import CreateUser from './CreateUser';
-import { BrowserRouter, Routes, Route,} from "react-router-dom";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Createproduct from './CreateProduct';
+import LogIn from './LogIn';
+import Portal from './Portal';
 
 function App() {
   return (
-   <BrowserRouter>
-    <div id="wrapper">
-      <SideBar></SideBar>
-      <div id="content-wrapper" class="d-flex flex-column">
-        <div id='content'>
-        <TopBar></TopBar>
-        <Routes>
-          <Route path='/dashboard' element={<Dashboard/>} />
-          <Route path='/users' element={<Users/>} />
-          <Route path='/create-user' element={<CreateUser/>} />
-          <Route path='/products' element={<Products/>} />
-        </Routes>
-       
+    <BrowserRouter>
+      <div id="wrapper">
+        <SideBar></SideBar>
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id='content'>
+            <TopBar></TopBar>
+            {/* <Outlet /> */}
+            <Routes>
+              {/* <Route path='/' element={<LogIn />} /> */}
+              {/* <Route path='/portal' element={<Portal />} > */}
+                <Route path='/dashboard' element={<Dashboard />} />
+                <Route path='/users' element={<Users />} />
+                <Route path='/create-user' element={<CreateUser />} />
+                <Route path='/products' element={<Products />} />
+                <Route path='/create-product' element={<Createproduct />} />
+              {/* </Route> */}
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
-   </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
---
+++ src/CreateProduct.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CreateProduct() {
+  return (
+    <div>Createproduct</div>
+  )
+}
+
+export default CreateProduct;
